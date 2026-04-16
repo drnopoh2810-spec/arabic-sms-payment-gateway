5b6a43c2b6cfcfce4a29debf5674410e614b54ae
drnopoh2810-spec <dr.nopoh2810@gmail.com>
2026-04-17 00:40:52 +0200

    Fix syntax error in WebService.kt - correct paymentRoutes() placement and bracket structure

--- app/src/main/java/me/capcom/smsgateway/modules/localserver/WebService.kt
+++ app/src/main/java/me/capcom/smsgateway/modules/localserver/WebService.kt
@@ -203,9 +203,9 @@ class WebService : Service() {
                     
                     // Payment routes
                     paymentRoutes()
-                    }
                 }
             }
+        }
         }
     }
 
